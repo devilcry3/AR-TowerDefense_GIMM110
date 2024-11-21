e22c9ba8540a019b101c1cda8426d930507ec620
devilcry3 <78716116+devilcry3@users.noreply.github.com>
2024-11-20 18:14:31 -0700

    Working on Spawn script
    have updated spawn script, haven't finished.
    added spawn point
    
    added  glow undead, and berserker unit

--- Assets/Scripts/Storm/EnemySpawn.cs
+++ Assets/Scripts/Storm/EnemySpawn.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +8,20 @@ public class EnemySpawn : MonoBehaviour
     // Regions help to visually organize your code into sections.
     #region Variables
     // Static variables are shared across all instances of a class
-    public static EnemySpawn Instance { get; private set; } // Singleton pattern that allows you to access the GameManager instance from other scripts
+    public static EnemySpawn Instance { get; private set; } // Singleton pattern that allows you to access the EnemySpawn instance from other scripts
 
     public bool isMethodCalled = false; //flags to track if a method was called or not
 
     // Headers are like titles for the Unity Inspector.
-
     [Header("Enemy Spawns")]
-    [SerializeField] GameObject enemyUndead;
+    [SerializeField] GameObject[] enemyUndead = new GameObject[3];
     [SerializeField] GameObject enemySpawnPos;
     [SerializeField] float timeToSpawn = 4f; // Time in seconds to spawn an enemy
-    [SerializeField] int maxEnemies = 15; // Maximum number of enemies that can be spawned
+    [SerializeField] int maxEnemies = 10; // Maximum number of enemies that can be spawned
+    [SerializeField] int wave = 1; //Indicates wave
 
     // HideInInspector hides the variable from the inspector, but allows other scripts to access it if needed
-    [HideInInspector] public int enemyCounter = 0; // Counter for the number of enemies spawned
+    [HideInInspector] public int unitCounter = 0; // Counter for the number of enemies spawned
     #endregion // Marks the end of the region
 
     #region Unity Methods
@@ -41,7 +42,22 @@ public class EnemySpawn : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        StartCoroutine(SpawnEnemy()); // Starts the coroutine to spawn enemies
+        switch (wave)
+        {
+            case 1:
+                StartCoroutine(SpawnTutorial());
+                break;
+            case 2:
+                maxEnemies = 20;
+                StartCoroutine(SpawnWave2());
+                break;
+            case 3:
+                StartCoroutine(SpawnWave3());
+                break;
+            default:
+                break;
+        }
+        // Starts the coroutine to spawn enemies
         //A coroutine is a method that can pause execution and return control to
         //Unity but then continue where it left off on the following frame.
         //if (SceneManager.GetActiveScene().buildIndex == 2)
@@ -52,21 +68,53 @@ public class EnemySpawn : MonoBehaviour
     }
     #endregion
 
-    #region Custom Methods
+    #region Coroutine Methods
     /// <summary>
     /// Spawns an enemy undead at regular intervals
-    /// </summary>
-    /// <returns></returns>
-    IEnumerator SpawnEnemy()
+    IEnumerator SpawnTutorial()
     {
         while (true) // Infinite loop to keep spawning enemies
         {
-            if (enemyCounter < maxEnemies)
+            if (unitCounter < maxEnemies)
             {
                 // Spawns an enemy undead at the enemy spawn position
-                GameObject spawnedEnemy = Instantiate(enemyUndead, enemySpawnPos.transform.position, Quaternion.identity, enemySpawnPos.transform);
+                GameObject spawnedEnemy = Instantiate(enemyUndead[0], enemySpawnPos.transform.position, Quaternion.identity, enemySpawnPos.transform);
                 spawnedEnemy.transform.Rotate(0, 180, 0); // Rotates the enemy to face the user
-                enemyCounter++;
+                unitCounter++;
+            }
+
+            yield return new WaitForSeconds(timeToSpawn); // Waits for the specified time before spawning another enemy
+        }
+    }
+
+    IEnumerator SpawnWave2()
+    {
+        while (true) // Infinite loop to keep spawning enemies
+        {
+            if (unitCounter < maxEnemies)
+            {
+                // Spawns an enemy undead at the enemy spawn position
+                GameObject spawnedEnemy = Instantiate(enemyUndead[1], enemySpawnPos.transform.position, Quaternion.identity, enemySpawnPos.transform);
+                spawnedEnemy.transform.Rotate(0, 180, 0); // Rotates the enemy to face the user
+                unitCounter++;
+            }
+
+            yield return new WaitForSeconds(timeToSpawn); // Waits for the specified time before spawning another enemy
+        }
+    }
+
+    IEnumerator SpawnWave3()
+    {
+        while (true) // Infinite loop to keep spawning enemies
+        {
+            if (unitCounter < maxEnemies)
+            {
+                // Spawns an enemy undead at the enemy spawn position
+                GameObject spawnedEnemy1 = Instantiate(enemyUndead[1], enemySpawnPos.transform.position, Quaternion.identity, enemySpawnPos.transform);
+                GameObject spawnedEnemy2 = Instantiate(enemyUndead[2], enemySpawnPos.transform.position, Quaternion.identity, enemySpawnPos.transform);
+                spawnedEnemy1.transform.Rotate(0, 180, 0); // Rotates the enemy to face the user
+                spawnedEnemy1.transform.Rotate(0, 180, 0);
+                unitCounter++;
             }
 
             yield return new WaitForSeconds(timeToSpawn); // Waits for the specified time before spawning another enemy
